45b259f6bee49a1b3640ec894ed5601a50df7f88
sanan90 <san-an@bk.ru>
2020-10-30 19:51:27 +0300

    HW2

--- app/src/main/java/ru/geekbains/hw_2/MainActivity.java
+++ app/src/main/java/ru/geekbains/hw_2/MainActivity.java
@@ -1,14 +1,40 @@
 package ru.geekbains.hw_2;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+}
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button1:
+                Intent intent = new Intent(this, MainActivity2.class);
+                startActivity(intent);
+                break;
+            case R.id.button2:
+                Intent intent2 = new Intent(this, MainActivity2.class);
+                startActivity(intent2);
+                break;
+            default:
+                break;
+        }
     }
 }
